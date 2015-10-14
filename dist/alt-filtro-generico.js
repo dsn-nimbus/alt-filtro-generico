530a92dd68892e2cddf1bb5f1a875afa729536a6
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-10-14 16:27:51 -0300

    dist

---
+++ dist/alt-filtro-generico.js
@@ -0,0 +1,88 @@
+;(function(ng) {
+  "use strict";
+
+  ng.module('alt.filtro-generico', [])
+    .factory('AltFiltro', [function () {
+      var AltFiltro = function(prop, valor) {
+        this.propriedade = [];
+        this.valor = [];
+
+        if (ng.isDefined(prop)) {
+          this.propriedade.push(prop);
+        }
+
+        if (ng.isDefined(valor)) {
+          this.valor.push(valor);
+        }
+      };
+
+      AltFiltro.prototype.adiciona = function(propriedade, valor) {
+        if (!propriedade) {
+          throw new Error('Propriedade deve ser informada para ser adicionada as propriedades.');
+        }
+
+        if (!valor) {
+          throw new Error('Valor deve ser informado para ser adicionado aos valores.');
+        }
+
+        this.propriedade.push(propriedade);
+        this.valor.push(valor);
+      };
+
+      AltFiltro.prototype.remove = function(propriedade) {
+        var _this = this;
+
+        ng.forEach(_this.propriedade, function(p, indice) {
+          if (p === propriedade) {
+            _this.propriedade.splice(indice, 1);
+            _this.valor.splice(indice, 1);
+          }
+        });
+      };
+
+      AltFiltro.prototype.removeTodos = function() {
+        this.propriedade.length = 0;
+        this.valor.length = 0;
+      };
+
+      AltFiltro.prototype.estaPreenchido = function() {
+        return !!this.propriedade.length && !!this.valor.length;
+      };
+
+      AltFiltro.prototype.formata = function() {
+        if (!this.estaPreenchido())
+        throw new Error('AltFiltro não está preenchido para ser formatado.');
+
+        return {propriedade: this.formataPropriedade(), valor: this.formataValor()};
+      };
+
+      AltFiltro.prototype.formataPropriedade = function() {
+        if (!this.estaPreenchido())
+        throw new Error('Propriedade não está preenchida para ser formatado.');
+
+        return this.propriedade.join();
+      };
+
+      AltFiltro.prototype.formataValor = function() {
+        if (!this.estaPreenchido())
+        throw new Error('Valor não está preenchido para ser formatado.');
+
+        return this.valor.join();
+      };
+
+      AltFiltro.prototype.seta = function(propriedade, valor) {
+        if (!propriedade) {
+          throw new Error('Propriedade deve ser informada para setar a propriedade.');
+        }
+
+        if (!valor) {
+          throw new Error('Valor deve ser informado para setar o valor.');
+        }
+
+        this.remove(propriedade);
+        this.adiciona(propriedade, valor);
+      };
+
+      return AltFiltro;
+    }]);
+}(angular));
